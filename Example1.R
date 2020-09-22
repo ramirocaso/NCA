9ece06f5502c8c59ac85f70fb8cd7dbea551d3c6
ramirocaso <31875438+ramirocaso@users.noreply.github.com>
2020-09-22 13:48:43 -0600

    Commit 3

--- Example1.R
+++ Example1.R
@@ -2,5 +2,9 @@ library(NCA)
 data("nca.example")
 data <-nca.example
 data <- data[-c(14,22,26),]
+
 model <- nca_analysis(data,c("Individualism", "Risk taking"),"Innovation performance")
 nca_output(model, plots=T)
+
+model2 <- nca_analysis(data,c("Risk taking", "Individualism"),"Innovation performance")
+nca_output(model2, plots=T)
